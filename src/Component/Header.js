ccdeb04881a8120e382bd049fbc4a6df7d05249b
mashahiruni12-a11y <mashahiruni12@gmail.com>
2025-09-25 14:59:36 +0530

    I created header and home page.

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,22 +1,43 @@
-// Header.js
 import React from 'react';
-import './Header.css'; // We will create this file for styling
+import './Header.css';
 
-const Header = () => {
+const Header = ({ activeSection, setActiveSection }) => {
   return (
     <header className="header-container">
-      <div className="header-content">
-        <h1>Savor the Exquisite Flavors of Dine Divine</h1>
-        <p>Experience Culinary Excellence in Every Bite</p>
-      </div>
       <nav className="navbar">
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/specials">Specials</a></li>
-          <li><a href="/menu">Menu</a></li>
-          <li><a href="/contact">Contact</a></li>
+        <div className="logo">Yummi Restaurant</div>
+        <ul className="nav-links">
+        
+          <li
+            className={activeSection === "Home" ? "active" : ""}
+            onClick={() => setActiveSection("Home")}
+          >
+            Home
+          </li>
+          <li
+            className={activeSection === "Specials" ? "active" : ""}
+            onClick={() => setActiveSection("Specials")}
+          >
+            Specials
+          </li>
+          <li
+            className={activeSection === "Menu" ? "active" : ""}
+            onClick={() => setActiveSection("Menu")}
+          >
+            Menu
+          </li>
+          <li
+            className={activeSection === "Contact" ? "active" : ""}
+            onClick={() => setActiveSection("Contact")}
+          >
+            Contact
+          </li>
         </ul>
       </nav>
+      <div className="header-content">
+        <h1>Savor the Exquisite Flavors of Yummi Restaurant</h1>
+        <p>Experience Culinary Excellence in Every Bite</p>
+      </div>
     </header>
   );
 };
